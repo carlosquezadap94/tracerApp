f0fbff024185c76a8f2f520859db7ea4fea04e9e
David Santacoloma <andres.santacoloma@ceiba.com.co>
2020-06-13 23:23:24 -0500

    Gral: Ajustar paquetes por cambios en clases

--- app/src/main/java/io/bluetrace/opentrace/services/CommandHandler.kt
+++ app/src/main/java/io/bluetrace/opentrace/services/CommandHandler.kt
@@ -2,6 +2,7 @@ package io.bluetrace.opentrace.services
 
 import android.os.Handler
 import android.os.Message
+import io.bluetrace.opentrace.services.enums.Command
 import java.lang.ref.WeakReference
 
 class CommandHandler(val service: WeakReference<BluetoothMonitoringService>) : Handler() {
@@ -9,50 +10,50 @@ class CommandHandler(val service: WeakReference<BluetoothMonitoringService>) : H
         msg?.let {
             //            val cmd = msg.arg1
             val cmd = msg.what
-            service.get()?.runService(BluetoothMonitoringService.Command.findByValue(cmd))
+            service.get()?.runService(Command.findByValue(cmd))
         }
     }
 
-    fun sendCommandMsg(cmd: BluetoothMonitoringService.Command, delay: Long) {
+    fun sendCommandMsg(cmd: Command, delay: Long) {
 //        val msg = obtainMessage(cmd.index)
         val msg = Message.obtain(this, cmd.index)
 //        msg.arg1 = cmd.index
         sendMessageDelayed(msg, delay)
     }
 
-    fun sendCommandMsg(cmd: BluetoothMonitoringService.Command) {
+    fun sendCommandMsg(cmd: Command) {
         val msg = obtainMessage(cmd.index)
         msg.arg1 = cmd.index
         sendMessage(msg)
     }
 
     fun startBluetoothMonitoringService() {
-        sendCommandMsg(BluetoothMonitoringService.Command.ACTION_START)
+        sendCommandMsg(Command.ACTION_START)
     }
 
     fun scheduleNextScan(timeInMillis: Long) {
         cancelNextScan()
-        sendCommandMsg(BluetoothMonitoringService.Command.ACTION_SCAN, timeInMillis)
+        sendCommandMsg(Command.ACTION_SCAN, timeInMillis)
     }
 
     fun cancelNextScan() {
-        removeMessages(BluetoothMonitoringService.Command.ACTION_SCAN.index)
+        removeMessages(Command.ACTION_SCAN.index)
     }
 
     fun hasScanScheduled(): Boolean {
-        return hasMessages(BluetoothMonitoringService.Command.ACTION_SCAN.index)
+        return hasMessages(Command.ACTION_SCAN.index)
     }
 
     fun scheduleNextAdvertise(timeInMillis: Long) {
         cancelNextAdvertise()
-        sendCommandMsg(BluetoothMonitoringService.Command.ACTION_ADVERTISE, timeInMillis)
+        sendCommandMsg(Command.ACTION_ADVERTISE, timeInMillis)
     }
 
     fun cancelNextAdvertise() {
-        removeMessages(BluetoothMonitoringService.Command.ACTION_ADVERTISE.index)
+        removeMessages(Command.ACTION_ADVERTISE.index)
     }
 
     fun hasAdvertiseScheduled(): Boolean {
-        return hasMessages(BluetoothMonitoringService.Command.ACTION_ADVERTISE.index)
+        return hasMessages(Command.ACTION_ADVERTISE.index)
     }
 }
