77ab56dff3b07b58c9c4601248ecfc5c76faffa7
David Santacoloma <andres.santacoloma@ceiba.com.co>
2020-06-15 20:18:36 -0500

    Gral: Agregar actividad y recursos para el splash

---
+++ app/src/main/java/io/bluetrace/opentrace/ui/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package io.bluetrace.opentrace.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.bluetrace.opentrace.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
