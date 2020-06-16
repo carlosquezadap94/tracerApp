82b5a7663ccf03096b8f9b8deba4bb656a81da1a
carlos quezada <carlos.quezada@ceiba.com.co>
2020-06-16 10:08:46 -0500

    Gral: Creacion de vista Main Menu y flujo con nueva vista

--- app/src/main/java/io/bluetrace/opentrace/ui/activity/VerificationActivity.kt
+++ app/src/main/java/io/bluetrace/opentrace/ui/activity/VerificationActivity.kt
@@ -1,12 +1,33 @@
 package io.bluetrace.opentrace.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import io.bluetrace.opentrace.R
+import kotlinx.android.synthetic.main.activity_verification.*
 
-class VerificationActivity : AppCompatActivity() {
+class VerificationActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verification)
+
+        setupUI()
+    }
+
+    private fun setupUI(){
+        buttonValidate.setOnClickListener(this)
+    }
+
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.buttonValidate->{
+                startActivity(
+                    Intent(this,
+                    MainMenuActivity::class.java)
+                )
+            }
+        }
     }
 }
