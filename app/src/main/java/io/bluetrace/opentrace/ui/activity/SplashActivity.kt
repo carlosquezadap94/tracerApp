8f366042418e01eef0443e5cacd6c079ca7d012e
David Santacoloma <andres.santacoloma@ceiba.com.co>
2020-06-15 21:13:47 -0500

    Gral: Agregar splash de la aplicación con animación Lottie

--- app/src/main/java/io/bluetrace/opentrace/ui/activity/SplashActivity.kt
+++ app/src/main/java/io/bluetrace/opentrace/ui/activity/SplashActivity.kt
@@ -1,12 +1,48 @@
 package io.bluetrace.opentrace.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
 import io.bluetrace.opentrace.R
+import io.bluetrace.opentrace.ui.StartActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var logoAnimation : LottieAnimationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setAnimation(this)
+    }
+
+    private fun setAnimation(context: Context) {
+        logoAnimation = findViewById(R.id.lottieAnimationView_splash_logoAnimation)
+
+        logoAnimation.addAnimatorListener(object :
+            Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+                Log.e("Animation:", "start")
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                Log.e("Animation:", "end")
+
+                val startActivityIntent = Intent(context, StartActivity::class.java)
+                startActivity(startActivityIntent)
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+                Log.e("Animation:", "cancel")
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+                Log.e("Animation:", "repeat")
+            }
+        })
     }
 }
