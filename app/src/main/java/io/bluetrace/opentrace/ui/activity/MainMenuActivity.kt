09f08d042f0fd91269ffc53631fc3b005cf585b8
Esteban Higuita - Ceiba Software <esteban.higuita@ceiba.com.co>
2020-06-16 11:04:23 -0500

    Gral: Agregar BorromNavigationView para menú inferior

--- app/src/main/java/io/bluetrace/opentrace/ui/activity/MainMenuActivity.kt
+++ app/src/main/java/io/bluetrace/opentrace/ui/activity/MainMenuActivity.kt
@@ -1,12 +1,19 @@
 package io.bluetrace.opentrace.ui.activity
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.bluetrace.opentrace.R
+import kotlinx.android.synthetic.main.activity_main_menu.*
 
-class MainMenuActivity : AppCompatActivity() {
+class MainMenuActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemReselectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+    }
+
+    override fun onNavigationItemReselected(item: MenuItem) {
+
     }
 }
